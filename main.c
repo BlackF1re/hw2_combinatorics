48b5a17e9f918b9b4056f0721282e7eda5ff77dd
Павел Левицкий <creative-creative@outlook.com>
2024-02-15 01:38:53 +0700

    upd

--- main.c
+++ main.c
@@ -1,35 +1,52 @@
 #include <stdio.h>
-#include <stdlib.h>
-
 #define MAX_SIZE 100
 
-void print_combination(int* arr, int k, int n) {
-    for (int i = 0; i < k; i++) {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
-}
+int c[MAX_SIZE + 1];
+int n, k;
 
-void generate_combinations(int* arr, int start, int index, int k, int n) {
-    if (index == k) {
-        print_combination(arr, k, n);
-        return;
+void combinationsGenerator() {
+    
+    int i, j;
+    //инициализация
+    for (i = 0; i <= k; i++) {
+        c[i] = i - 1;
     }
 
-    for (int i = start; i < n - k + index + 1; i++) {
-        arr[index] = i;
-        generate_combinations(arr, i + 1, index + 1, k, n);
+    //генерация
+    while (1) {
+        for (i = 0; i <= k; i++) {
+            if (i > 0) {
+                printf("%d ", c[i] + 1);
+            }
+        }
+        printf("\n");
+
+        //поиск первого элемента для инкремента
+        j = k;
+        while (j >= 0 && c[j] == n - k + j - 1) {
+            j--;
+        }
+        if (j == 0) {
+            break;
+        }
+
+        //инкремент первого элемента
+        c[j]++;
+
+        //сброс всех элементов вправо
+        for (i = j + 1; i <= k; i++) {
+            c[i] = c[i - 1] + 1;
+        }
     }
 }
 
 void error() {
-    printf("Возникла ошибка.");
+    printf("An error occured.");
     exit(1);
 }
 
 int main() {
     SetConsoleOutputCP(1251);
-    int n, k;
 
     printf("Введите мощность множества: ");
     int isDigit = scanf("%d", &n);
@@ -41,13 +58,12 @@ int main() {
     if (isDigit != 1)
         error();
 
-    if (k > n || k <= 0 || n <= 0)
-        error();
+    if (k > n || k <= 0 || n <= 0 || k > 100 || n> 100) {
+        printf("Введены некорректные значения.\n");
+        exit(1);
+    }
 
-    int* arr = (int*)malloc(k * sizeof(int));
+    combinationsGenerator();
 
-    generate_combinations(arr, 0, 0, k, n);
-
-    free(arr);
     return 0;
 }
